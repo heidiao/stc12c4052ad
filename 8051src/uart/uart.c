fff56069b64355051e20b5ea8f387710058c17e6
heidi <ophion126@gmail.com>
2015-10-12 08:27:34 +0800

    Add 8051 hex code, uart source, client-side python script

---
+++ 8051src/uart/uart.c
@@ -0,0 +1,24 @@
+#include <STC12C2052AD.H>
+void UART_init (void){ 
+    TMOD = 0x20;   
+    SCON = 0x50; 
+    TH1 = 0xF3; 
+    TL1 = 0xF3; 
+    PCON = 0x80;   
+    TR1 = 1; 
+}
+
+void main (void){
+    unsigned char UART_data;  
+    UART_init();
+    while(1){
+        if (RI == 1){        
+            UART_data = SBUF;   
+            RI = 0;      
+
+            SBUF = UART_data;  
+            while(TI == 0); 
+            TI = 0; 
+        }
+    }
+}
---
+++ pc/uart.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+import serial, time
+#initialization and open the port
+#possible timeout values:
+#    1. None: wait forever, block call
+#    2. 0: non-blocking mode, return immediately
+#    3. x, x is bigger than 0, float allowed, timeout block call
+ser = serial.Serial()
+ser.port = "/dev/ttyUSB0"
+ser.baudrate = 4800
+ser.bytesize = serial.EIGHTBITS #number of bits per bytes
+ser.parity = serial.PARITY_NONE #set parity check: no parity
+ser.stopbits = serial.STOPBITS_ONE #number of stop bits
+#ser.timeout = None          #block read
+ser.timeout = 0             #non-block read
+#ser.timeout = 2              #timeout block read
+ser.xonxoff = False     #disable software flow control
+ser.rtscts = False     #disable hardware (RTS/CTS) flow control
+ser.dsrdtr = False       #disable hardware (DSR/DTR) flow control
+ser.writeTimeout = 2     #timeout for write
+try: 
+    ser.open()
+except Exception, e:
+    print "error open serial port: " + str(e)
+    exit()
+if ser.isOpen():
+    try:
+        ser.flushInput() #flush input buffer, discarding all its contents
+        ser.flushOutput()#flush output buffer, aborting current output 
+                     #and discard all that is in buffer
+        #write data
+	#msg = "hello\x0D"
+	msg = "hello"
+        ser.write(msg)
+	print msg
+        time.sleep(0.5)  #give the serial port sometime to receive the data
+        numOfLines = 0
+        while True:
+            response = ser.readline()
+            print("read data: [" + response)
+	    if response == "":
+		break;
+        ser.close()
+    except Exception, e1:
+        print "error communicating...: " + str(e1)
+else:
+    print "cannot open serial port "
